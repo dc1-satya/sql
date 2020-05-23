84f40c3dee3b7348b9b0dd9c46a3b8a1cb1fa1fd
dc1-satya <dc1.satya@gmail.com>
2020-05-23 19:17:55 +0530

    1 file modified

--- Sample Databases/Empoyee Database/01.sql
+++ Sample Databases/Empoyee Database/01.sql
@@ -23,5 +23,23 @@ CREATE TABLE departments (
     UNIQUE  KEY (dept_name)                -- Build INDEX on this unique-value column
 );
 
+-- 3 Table "dept_emp"
+Junction table to support between many-to-many relationship between employees and departments. A department has many employees. An employee can belong to different department at different dates, and possibly concurrently.
 
+CREATE TABLE dept_emp (
+    emp_no      INT         NOT NULL,
+    dept_no     CHAR(4)     NOT NULL,
+    from_date   DATE        NOT NULL,
+    to_date     DATE        NOT NULL,
+    KEY         (emp_no),   -- Build INDEX on this non-unique-value column
+    KEY         (dept_no),  -- Build INDEX on this non-unique-value column
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+           -- Cascade DELETE from parent table 'employee' to this child table
+           -- If an emp_no is deleted from parent 'employee', all records
+           --  involving this emp_no in this child table are also deleted
+           -- ON UPDATE CASCADE??
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+           -- ON UPDATE CASCADE??
+    PRIMARY KEY (emp_no, dept_no)
+       
 
