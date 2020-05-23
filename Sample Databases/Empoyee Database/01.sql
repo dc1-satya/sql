e3e2b46873d1876092415d31be1a3ba8b7881c90
dc1-satya <dc1.satya@gmail.com>
2020-05-23 19:15:52 +0530

    1 file added

---
+++ Sample Databases/Empoyee Database/01.sql
@@ -0,0 +1,27 @@
+https://www.ntu.edu.sg/home/ehchua/programming/sql/SampleDatabases.html
+MySQL
+
+-- 1 Table "employees"
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,  -- UNSIGNED AUTO_INCREMENT??
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,  -- Enumeration of either 'M' or 'F'  
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)                   -- Index built automatically on primary-key column
+                                           -- INDEX (first_name)
+                                           -- INDEX (last_name)
+);
+
+
+-- 2 Table "departments"
+CREATE TABLE departments (
+    dept_no     CHAR(4)         NOT NULL,  -- in the form of 'dxxx'
+    dept_name   VARCHAR(40)     NOT NULL,
+    PRIMARY KEY (dept_no),                 -- Index built automatically
+    UNIQUE  KEY (dept_name)                -- Build INDEX on this unique-value column
+);
+
+
+
